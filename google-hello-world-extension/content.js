ac757b3085115c223740523f39d73dd57f8e5b66
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-12-29 12:35:56 +0900

    save

--- google-hello-world-extension/content.js
+++ google-hello-world-extension/content.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var helloWorldDiv = document.createElement('div');
-  helloWorldDiv.innerText = 'Hello World';
-  helloWorldDiv.style.position = 'fixed';
-  helloWorldDiv.style.top = '10px';
-  helloWorldDiv.style.left = '10px';
-  helloWorldDiv.style.backgroundColor = 'white';
-  helloWorldDiv.style.padding = '10px';
-  helloWorldDiv.style.zIndex = '1000';
-  document.body.appendChild(helloWorldDiv);
-});
+var helloWorldDiv = document.createElement('div');
+helloWorldDiv.innerText = 'Hello World';
+helloWorldDiv.style.position = 'fixed';
+helloWorldDiv.style.top = '10px';
+helloWorldDiv.style.left = '10px';
+helloWorldDiv.style.backgroundColor = 'white';
+helloWorldDiv.style.padding = '10px';
+helloWorldDiv.style.zIndex = '1000';
+document.body.appendChild(helloWorldDiv);
