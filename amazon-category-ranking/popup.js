205d5e285df00650bf821b7de07df7beabe2c199
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-12-29 13:28:28 +0900

    save

--- amazon-category-ranking/popup.js
+++ amazon-category-ranking/popup.js
@@ -2,7 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {action: "getCategory"}, function(response) {
       if (response && response.categoryHtml) {
-        document.getElementById('result').innerHTML = 'Category Ranking: <br>' + response.categoryHtml;
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = 'Category Ranking: <br>' + response.categoryHtml;
+
+        // リンクを新しいタブで開くようにイベントリスナーを設定
+        const links = resultDiv.getElementsByTagName('a');
+        for (let i = 0; i < links.length; i++) {
+          links[i].addEventListener('click', function(e) {
+            e.preventDefault(); // デフォルトのリンク動作を防止
+            chrome.tabs.create({url: e.target.href});
+          });
+        }
       } else {
         document.getElementById('result').textContent = 'Category ranking not found.';
       }
