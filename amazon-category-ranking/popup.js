31eeed5f7b6171805a3ec1db9a1dfeca96ec7ccc
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-12-29 13:41:56 +0900

    save

--- amazon-category-ranking/popup.js
+++ amazon-category-ranking/popup.js
@@ -1,21 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "getCategory"}, function(response) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "getCategory" }, function(response) {
       if (response && response.categoryHtml) {
-        const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = 'Category Ranking: <br>' + response.categoryHtml;
+        const formattedHtml = formatCategoryHtml(response.categoryHtml);
+        document.getElementById('result').innerHTML = formattedHtml;
 
-        // リンクを新しいタブで開くようにイベントリスナーを設定
-        const links = resultDiv.getElementsByTagName('a');
-        for (let i = 0; i < links.length; i++) {
-          links[i].addEventListener('click', function(e) {
-            e.preventDefault(); // デフォルトのリンク動作を防止
-            chrome.tabs.create({url: e.target.href});
+        // リンクにイベントリスナーを追加して、新しいタブで開くようにする
+        Array.from(document.querySelectorAll('#result a')).forEach(link => {
+          link.addEventListener('click', function(e) {
+            e.preventDefault();
+            chrome.tabs.create({ url: this.href });
           });
-        }
+        });
       } else {
         document.getElementById('result').textContent = 'Category ranking not found.';
       }
     });
   });
 });
+
+function formatCategoryHtml(html) {
+  // HTML文字列をDOMノードに変換
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = html;
+
+  // ランキング情報を整形
+  let formattedHtml = '<div class="ranking-title">Amazon Category Ranking:</div>';
+  const rankings = tempDiv.querySelectorAll('a');
+
+  rankings.forEach(link => {
+    if (link.href) {
+      // 相対リンクを絶対リンクに変換
+      const absoluteLink = link.href.startsWith('/') ? `https://www.amazon.co.jp${link.href}` : link.href;
+      // リンクテキストと組み合わせて整形
+      formattedHtml += `<div class="ranking"><a href="${absoluteLink}" class="category-link">${link.textContent}</a></div>`;
+    }
+  });
+
+  return formattedHtml;
+}
