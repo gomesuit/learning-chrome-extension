984862a5416e667f0d445d28013a8d7d88c970e9
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-12-29 13:32:38 +0900

    save

--- amazon-category-ranking/content.js
+++ amazon-category-ranking/content.js
@@ -1,14 +1,23 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "getCategory") {
-    const categoryElement = document.querySelector('#detailBulletsWrapper_feature_div > ul:nth-child(4) > li > span');
-    if (categoryElement) {
-      let categoryHtml = categoryElement.innerHTML;
-      // 相対パスのリンクを絶対パスに変換
-      categoryHtml = categoryHtml.replace(/href="\//g, 'href="https://www.amazon.co.jp/');
-      sendResponse({ categoryHtml: categoryHtml });
-    } else {
-      sendResponse({ categoryHtml: null });
+    // 複数のセレクタを試す
+    const selectors = [
+      '#detailBulletsWrapper_feature_div > ul:nth-child(4) > li > span',
+      '#productDetails_detailBullets_sections1 > tbody > tr:nth-child(3) > td'
+    ];
+
+    for (let i = 0; i < selectors.length; i++) {
+      const categoryElement = document.querySelector(selectors[i]);
+      if (categoryElement) {
+        let categoryHtml = categoryElement.innerHTML;
+        categoryHtml = categoryHtml.replace(/href="\//g, 'href="https://www.amazon.co.jp/');
+        sendResponse({ categoryHtml: categoryHtml });
+        return true;
+      }
     }
+
+    // 適切な情報が見つからない場合
+    sendResponse({ categoryHtml: null });
   }
-  return true; // 非同期レスポンスのために true を返す
+  return true;
 });
